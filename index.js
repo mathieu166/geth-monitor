a4f5327eec78651c5715752cc77feb90fa265ca3
Mathieu Riendeau <riendeau_mathieu@hotmail.com>
2024-08-12 19:11:26 -0400

    adding last mined block time check

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 const express = require('express');
-const {Web3} = require('web3');
+const { Web3 } = require('web3');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
 const app = express();
 const port = 3001;
 
+// Load environment variables
+const dbPath = process.env.GETH_STATS_DB_PATH;
+const minerAddress = process.env.MINER_ADDRESS;
+
+// Function to get the last timestamp for a given miner address
+const getLastTimestamp = (dbPath, minerAddress) => {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(dbPath, (err) => {
+      if (err) {
+        return reject(err);
+      }
+    });
+
+    const query = `SELECT timestamp FROM block WHERE lower(miner_address) = lower(?) ORDER BY timestamp DESC LIMIT 1`;
+
+    db.get(query, [minerAddress], (err, row) => {
+      db.close();
+      if (err) {
+        return reject(err);
+      }
+      if (row) {
+        resolve(row.timestamp);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+};
+
 // Define the endpoint to check Geth status
 app.get('/check-geth', async (req, res) => {
   try {
+    if (dbPath && minerAddress) {
+      // Check the database for the last timestamp
+      const lastTimestamp = await getLastTimestamp(dbPath, minerAddress);
+      const oneHourAgo = Math.floor(Date.now() / 1000) - 3600;
+      
+      console.log(lastTimestamp, oneHourAgo)
+      if (lastTimestamp && lastTimestamp < oneHourAgo) {
+        res.status(500).send('Geth not validating anymore');
+        return;
+      }
+    }
+
     // Create a new Web3 instance for each request
     const web3 = new Web3('http://localhost:8545');
 
@@ -20,7 +63,7 @@ app.get('/check-geth', async (req, res) => {
       res.status(500).send('Geth is not connected to any peers.');
     }
   } catch (error) {
-    res.status(500).send(`Error connecting to Geth: ${error.message}`);
+    res.status(500).send(`Error: ${error.message}`);
   }
 });
 
